ad216afbf51fbb149976e4112ff141b50afdf5f4
OceanOC <141111946+OceanOC@users.noreply.github.com>
2024-12-11 16:03:46 +0530

    Add C-Mini.c template to C-GNUMake-Minimal

--- c.go
+++ c.go
@@ -3,15 +3,10 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 )
-
-//go:embed templates/Makefile-C
-var mfc string
 
 func cMakeMini(name, cflags, ldflags, cc string) {
 	namec := filepath.Clean(name)
@@ -23,23 +18,19 @@ func cMakeMini(name, cflags, ldflags, cc string) {
 		fmt.Println("Could not create Makefile", err.Error())
 	}
 
-	var mfs string
-	body, err := http.Get("https://raw.githubusercontent.com/OceanOC/projxgen/refs/heads/main/templates/Makefile-C")
-	if err != nil || body.StatusCode != http.StatusOK {
-		// Incase of any errors use embedded Makefile-C
-		mfs = mfc
-	} else {
-		bodyBytes, err := io.ReadAll(body.Body)
-		if err != nil {
-			mfs = mfc
-		} else {
-			mfs = string(bodyBytes)
-		}
+	cf, err := os.Create(filepath.Join(namec, "src", "main.c"))
+	if err != nil {
+		fmt.Println("Could not create main.c", err.Error())
 	}
+
+	mfs := getTemplate(Makefile_C)
 	mfs = strings.ReplaceAll(mfs, ".$PROJNAME", name)
 	mfs = strings.ReplaceAll(mfs, ".$CFLAGS", cflags)
 	mfs = strings.ReplaceAll(mfs, ".$LDFLAGS", ldflags)
 	mfs = strings.ReplaceAll(mfs, ".$CC", cc)
-
 	mf.WriteString(mfs)
+
+	mcs := getTemplate(C_Mini)
+	mcs = strings.ReplaceAll(mcs, ".$PROJNAME", name)
+	cf.WriteString(mcs)
 }
