98c28e2196186ea6a012c1e912da351ec4f3f968
OceanOC <141111946+OceanOC@users.noreply.github.com>
2024-12-15 16:30:54 +0530

    Use Usage instead of Description

--- main.go
+++ main.go
@@ -22,20 +22,20 @@ func main() {
 				Usage:   "Make a new project",
 				Commands: []*cli.Command{
 					{
-						Name:        "C-GNUMake-Minimal",
-						Description: "A minimal C project with GNU Make build system",
-						Category:    "C",
-						Aliases:     []string{"c", "c-make-min"},
-						Flags:       cFlags(&fv),
+						Name:     "C-GNUMake-Minimal",
+						Usage:    "A minimal C project with GNU Make build system",
+						Category: "C",
+						Aliases:  []string{"c", "c-make-min"},
+						Flags:    cFlags(&fv),
 						Action: func(ctx context.Context, cmd *cli.Command) error {
 							return cMakeMini(cmd.Args().Get(0), strings.Join(fv.cflags, " --")+" --cstd="+fv.cver, strings.Join(fv.ldflags, " "), fv.compiler)
 						},
 					},
 					{
-						Name:        "C-Minimal",
-						Description: "A minimal C project with no build system",
-						Category:    "C",
-						Aliases:     []string{"c-min"},
+						Name:     "C-Minimal",
+						Usage:    "A minimal C project with no build system",
+						Category: "C",
+						Aliases:  []string{"c-min"},
 						Action: func(ctx context.Context, cmd *cli.Command) error {
 							return cMini(cmd.Args().Get(0))
 						},
